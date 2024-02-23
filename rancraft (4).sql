2365ff822cfd5f597fd9f2c0c2ecda2095d23e4a
bebra678 <daniilkatrasov@gmail.com>
2024-02-23 22:05:14 +0700

    v3.1 мелкие изменения

--- rancraft (4).sql
+++ rancraft (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 15 2024 г., 13:11
+-- Время создания: Фев 23 2024 г., 16:04
 -- Версия сервера: 10.4.28-MariaDB
 -- Версия PHP: 8.2.4
 
@@ -60,7 +60,8 @@ INSERT INTO `bans` (`id`, `bannable_type`, `bannable_id`, `created_by_type`, `cr
 (13, 'App\\Models\\User', 17, NULL, NULL, NULL, NULL, '2024-02-11 14:05:17', '2024-02-11 13:47:21', '2024-02-11 14:05:17'),
 (14, 'App\\Models\\User', 17, NULL, NULL, NULL, NULL, '2024-02-11 14:05:17', '2024-02-11 14:03:22', '2024-02-11 14:05:17'),
 (15, 'App\\Models\\User', 17, NULL, NULL, 'Читы', '2029-12-31 17:00:00', '2024-02-11 14:05:17', '2024-02-11 14:03:50', '2024-02-11 14:05:17'),
-(16, 'App\\Models\\User', 17, NULL, NULL, 'Читыasd', '2029-12-31 17:00:00', '2024-02-11 14:09:24', '2024-02-11 14:08:11', '2024-02-11 14:09:24');
+(16, 'App\\Models\\User', 17, NULL, NULL, 'Читыasd', '2029-12-31 17:00:00', '2024-02-11 14:09:24', '2024-02-11 14:08:11', '2024-02-11 14:09:24'),
+(17, 'App\\Models\\User', 17, NULL, NULL, 'Читы', '2029-12-31 17:00:00', '2024-02-23 07:59:30', '2024-02-23 07:59:09', '2024-02-23 07:59:30');
 
 -- --------------------------------------------------------
 
@@ -71,6 +72,7 @@ INSERT INTO `bans` (`id`, `bannable_type`, `bannable_id`, `created_by_type`, `cr
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `nick` varchar(255) NOT NULL,
   `post_id` int(11) NOT NULL,
   `message` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -81,9 +83,9 @@ CREATE TABLE `comments` (
 -- Дамп данных таблицы `comments`
 --
 
-INSERT INTO `comments` (`id`, `user_id`, `post_id`, `message`, `created_at`, `updated_at`) VALUES
-(1, 1, 11, 'Коммент', '2024-02-14 14:16:48', NULL),
-(2, 5, 11, 'Бла бла', '2024-02-14 14:13:13', NULL);
+INSERT INTO `comments` (`id`, `user_id`, `nick`, `post_id`, `message`, `created_at`, `updated_at`) VALUES
+(1, 1, 'user_123', 11, 'Коммент', '2024-02-23 13:59:57', NULL),
+(12, 27, 'admin', 10, 'Блла бла', '2024-02-23 07:09:29', '2024-02-23 07:09:29');
 
 -- --------------------------------------------------------
 
@@ -97,7 +99,7 @@ CREATE TABLE `donates` (
   `nick` varchar(255) NOT NULL,
   `donate` varchar(255) NOT NULL,
   `type_payment` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `contact` varchar(255) NOT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -106,11 +108,15 @@ CREATE TABLE `donates` (
 -- Дамп данных таблицы `donates`
 --
 
-INSERT INTO `donates` (`id`, `user_id`, `nick`, `donate`, `type_payment`, `status`, `updated_at`, `created_at`) VALUES
-(1, 1, 'user_123', '1', 'Карта', 0, NULL, NULL),
-(2, 2, 'asdd_123', '0', 'Крипта', 0, '2024-01-29 07:08:29', '2024-01-29 07:08:29'),
-(3, 2, 'asdd_123', '0', 'Крипта', 0, '2024-01-29 07:08:50', '2024-01-29 07:08:50'),
-(4, 2, 'asdd_123', '0', 'Крипта', 0, '2024-01-29 07:10:13', '2024-01-29 07:10:13');
+INSERT INTO `donates` (`id`, `user_id`, `nick`, `donate`, `type_payment`, `contact`, `updated_at`, `created_at`) VALUES
+(1, 1, 'user_123', '1', 'Карта', '', NULL, NULL),
+(2, 2, 'asdd_123', '0', 'Крипта', '', '2024-01-29 07:08:29', '2024-01-29 07:08:29'),
+(3, 2, 'asdd_123', '0', 'Крипта', '', '2024-01-29 07:08:50', '2024-01-29 07:08:50'),
+(4, 2, 'asdd_123', '0', 'Крипта', '', '2024-01-29 07:10:13', '2024-01-29 07:10:13'),
+(7, 1, 'user_123', '1', 'Карта', '', '2024-02-15 06:00:29', '2024-02-15 06:00:29'),
+(8, 1, 'user_123', 'Приват', 'Карта', '', '2024-02-15 06:53:29', '2024-02-15 06:53:29'),
+(9, 27, 'admin', 'Приват', 'Карта', 'vk: @id123123123123', '2024-02-22 10:26:40', '2024-02-22 10:26:40'),
+(10, 27, 'admin', 'Приват', 'Карта', 'vk: @id123123123123', '2024-02-22 10:47:17', '2024-02-22 10:47:17');
 
 -- --------------------------------------------------------
 
@@ -238,13 +244,10 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `text`, `photo`, `created_at`, `updated_at`) VALUES
-(10, 'Новость 1', 'Бла бла', 'images/vShA5lGJmG90b9oKpDlHLZZVq2CjOy65Oi3BZBPO.png', '2024-02-04 11:12:12', '2024-02-04 11:12:12'),
-(11, 'Новость 2', 'Бла бла', 'images/TKrXhXk9791ybx46ZW8B6xfCvT68oab7WgzG6XnY.png', '2024-02-04 11:14:10', '2024-02-04 11:14:10'),
-(12, 'Новость 3', 'Бла бла', 'images/Wn1deUmnCr3lMGq1czlVSpK6IThT9cc9I2g9qesf.png', '2024-02-04 11:16:07', '2024-02-04 11:16:07'),
-(13, 'Новость 4', 'Бла бла', 'images/FQ2D7KcrXCsKMzktM4TnLFO2mAGh4GJ6doZ56aIl.png', '2024-02-04 11:17:12', '2024-02-04 11:17:12'),
-(14, 'Новость 5', 'Бла блаj', 'images/FRn0ibc3I0ialWe8QMd14doNFA89mMEW7jigS8y9.png', '2024-02-04 11:18:01', '2024-02-04 11:18:01'),
-(15, 'Новость 6', 'Бла бла', 'images/FS3FW5eDBNIDEYDoCUd49DPSush3B52gOkJgEKEc.jpg', '2024-02-07 04:47:39', '2024-02-07 04:47:39'),
-(16, 'Новость 7', 'Бла бла', 'images/91C26d0HeSHJQMvlXTBpvr8cjzGMHoFyWjqG6AoJ.jpg', '2024-02-07 04:49:15', '2024-02-07 04:49:15');
+(10, 'Новость 1', 'Бла бла', 'post/r5fFrRXyXsfjucqJZRhT5xnqmWhAQUlZlqVywahB.jpg', '2024-02-04 11:12:12', '2024-02-04 11:12:12'),
+(11, 'Новость 2', 'Бла бла', 'post/r5fFrRXyXsfjucqJZRhT5xnqmWhAQUlZlqVywahB.jpg', '2024-02-04 11:14:10', '2024-02-04 11:14:10'),
+(12, 'Новость 3', 'Бла бла', 'post/r5fFrRXyXsfjucqJZRhT5xnqmWhAQUlZlqVywahB.jpg', '2024-02-04 11:16:07', '2024-02-04 11:16:07'),
+(13, 'Новость 4', 'Бла бла', 'post/r5fFrRXyXsfjucqJZRhT5xnqmWhAQUlZlqVywahB.jpg', '2024-02-04 11:17:12', '2024-02-04 11:17:12');
 
 -- --------------------------------------------------------
 
@@ -274,8 +277,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nick`, `name`, `email`, `email_verified_at`, `password`, `role`, `photo`, `remember_token`, `tg`, `vk`, `created_at`, `updated_at`, `banned_at`) VALUES
-(1, 'user_123', 'Username', 'user@mail.ru', NULL, '$2y$12$vILFA8HQ6STQBefhWyUh7O0J39/CysP5ZLf4mNw5ra.MovI.UuE36', NULL, NULL, NULL, NULL, NULL, '2024-01-22 09:17:20', '2024-02-05 14:09:59', NULL),
-(2, 'johan123', 'John', 'john@mail.ru', NULL, '$2y$12$kkquw/EH2R7LEMxwK55dbu3zOGKk7vuaZzJHyq7gIXMIw9pwyk1uG', NULL, NULL, NULL, NULL, NULL, '2024-01-22 10:43:48', '2024-02-05 14:35:03', '2024-02-05 14:35:03'),
+(1, 'user_123', 'Username', 'user@mail.ru', NULL, '$2y$12$Ktb5pSOIafT9hSDvZfqs8eQDGbK8hs97brCAnG4Ti2TQB1bbZvhaK', NULL, NULL, NULL, NULL, NULL, '2024-01-22 09:17:20', '2024-02-22 07:05:34', NULL),
+(2, 'johan123', 'John', 'john@mail.ru', NULL, '$2y$12$kB.1C9l4Somx20Ufynb0aeRE2Hvsb0j/JEefjJCXf7RxCpHdSDx9u', NULL, NULL, NULL, NULL, NULL, '2024-01-22 10:43:48', '2024-02-22 06:31:02', '2024-02-05 14:35:03'),
 (4, 'aboba_123', 'Aboba', 'aboba@mail.ru', NULL, '$2y$12$fHa0qv88IN2Drg9LuVZ7ZuwaBFvl/xSDqSPqhKwP/XY.4RllBnluO', NULL, NULL, NULL, NULL, NULL, '2024-01-23 05:54:04', '2024-02-05 14:37:42', '2024-02-05 14:37:42'),
 (5, 'nick_123', 'nick', 'nick@mail.ru', NULL, '$2y$12$k.AbPQATeuwnQceERGyUz.b8TP8wcQaaUvyGqHOVR2ddb5k/Zoptu', NULL, NULL, NULL, NULL, NULL, '2024-01-23 05:56:43', '2024-02-05 14:38:24', '2024-02-05 14:38:24'),
 (7, 'fgsad_123', 'asfd', 'sdgsdg@mail.ru', NULL, '$2y$12$V1tJU7q.smMHKngASpGk6.NbKtvjVh1qF3xd0xOPL9CcO4gWS3LJC', NULL, NULL, NULL, NULL, NULL, '2024-01-23 06:05:55', '2024-02-05 14:39:48', '2024-02-05 14:39:48'),
@@ -288,16 +291,16 @@ INSERT INTO `users` (`id`, `nick`, `name`, `email`, `email_verified_at`, `passwo
 (14, 'asdasd123', 'adsdasd2d', 'dasf2r@mail.rd', NULL, '$2y$12$JvLqc/s7KIpOr7t0QBHj9OY0lkII0jsKHIcKbwBIZLJE23B7cpa1W', NULL, NULL, NULL, NULL, NULL, '2024-01-27 23:02:39', '2024-01-27 23:02:39', NULL),
 (15, 'dfghfghhsd34', 'asd', 'daniilkatrasov@gmail.com', NULL, '$2y$12$nNXsFZytph0uHyGYRBT/VeNpl0kR1hebw7ViHzCyWuYxdKfqithxu', NULL, NULL, NULL, NULL, NULL, '2024-01-27 23:35:12', '2024-02-11 13:25:43', NULL),
 (16, 'asdfsdggzsdgsdgsdg', 'asdfs', 'dsadasdafhjg2@mail.ru', NULL, '$2y$12$QR6HTvXyNd0nOcykXloyv.8TLTIoNtPXCHTJevpCHliUlVekXxtIK', NULL, NULL, NULL, NULL, NULL, '2024-01-28 04:27:16', '2024-02-11 13:40:40', '2024-02-11 13:40:40'),
-(17, 'asdasdg43y34', '0dsafgeg', 'asfasfasfjjljul@mail.ru', NULL, '$2y$12$qQE0qnqG7dZjbygf.MKZae2wMVgbVHUhvLy.8qTOEel4EKYBZUo8m', NULL, NULL, NULL, NULL, NULL, '2024-01-29 05:03:39', '2024-02-11 14:09:24', NULL),
+(17, 'asdasdg43y34', '0dsafgeg', 'asfasfasfjjljul@mail.ru', NULL, '$2y$12$qQE0qnqG7dZjbygf.MKZae2wMVgbVHUhvLy.8qTOEel4EKYBZUo8m', NULL, NULL, NULL, NULL, NULL, '2024-01-29 05:03:39', '2024-02-23 07:59:30', NULL),
 (18, 'dasdasddsaf21', 'sdfgsdg', 'asdafgi34@mail.ru', NULL, '$2y$12$FnKkRXPZ0yGWGpe7WUUDcuX8dfMmqeTsp/t/J8KmBRDUt6Cs2z/xW', NULL, NULL, NULL, NULL, NULL, '2024-01-29 05:54:00', '2024-01-29 05:54:00', NULL),
 (19, 'sdgsdgsdgaw12d_123', 'asdasdf', 'safasffas22d12@mail.com', NULL, '$2y$12$stog8TRaNw5dILqiw9GIe.NJHm9AccTTv08lo17Y6wDEtoVrWXnym', NULL, NULL, NULL, NULL, NULL, '2024-02-04 08:23:40', '2024-02-04 08:23:40', NULL),
 (20, 'sdfgjdgshuo2', 'asffasf', 'phpasdstorm889@gmail.comasdsdas', NULL, '$2y$12$CXGYBVZ5vwsSfKy/AHwOUOr9Q4VwTvnlScoUx2eg0u5jRhdsbRzay', NULL, NULL, NULL, NULL, NULL, '2024-02-04 08:52:08', '2024-02-04 08:52:08', NULL),
 (21, 'saffsfff22f2f2f', 'asdasd', 'phpstroasdsddm889@gmail.com', NULL, '$2y$12$DVVbqZQIqo1BkMzM5956OuvC.7tErjE0duYObGTDJLdDXx2ActNIu', NULL, NULL, NULL, NULL, NULL, '2024-02-04 08:54:04', '2024-02-04 08:54:04', NULL),
 (22, 'test1', 'asdasdasdasg3', 'phpstorasdm889@gmail.com', NULL, '$2y$12$bjrR86JM99T8y28AFMGm2.t18C/kcrK01ZzNdHL4b2nYbU.qai.wa', NULL, NULL, NULL, NULL, NULL, '2024-02-04 08:55:35', '2024-02-04 08:55:35', NULL),
-(23, 'jsdjhsdhh2', 'asf', 'sdgjkljklhhjl3@mail.com', NULL, '$2y$12$RwxiD./YBZaUpMiZxcyAS.ZC09k0dlr6FvAiVL8uWg.gy2XTRKg82', NULL, 'images/OhS1MtpwFgIam8PQRybMNjIItIXFH322OtYlXUAH.jpg', NULL, NULL, NULL, '2024-02-04 08:56:42', '2024-02-04 11:48:31', NULL),
+(23, 'jsdjhsdhh2', 'asf', 'sdgjkljklhhjl3@mail.com', NULL, '$2y$12$KhrH.HyLG7ijgu8AAkyDiuueE/QwMp.QRdPRjI884zpEJSjE3yv6e', NULL, 'images/OhS1MtpwFgIam8PQRybMNjIItIXFH322OtYlXUAH.jpg', NULL, NULL, NULL, '2024-02-04 08:56:42', '2024-02-22 09:26:51', NULL),
 (25, 'test4', 'asdasd', 'sony996525@gmail.com', NULL, '$2y$12$iKib1hV/ohz5iKT8CSxe0.RMKzBWztZfXO2ZcnNEsrCbc.ISmQdte', NULL, NULL, NULL, NULL, NULL, '2024-02-04 09:00:32', '2024-02-04 09:00:32', NULL),
 (26, 'test5', 'asf', 'sdgjkljk23lhhjl3@mail.com', NULL, '$2y$12$J6buwvUp09JWiFynniNOp.HNbsjF/pVa0RHexvecGqY1C1WBM9Yki', NULL, 'images/vk5TqeT50MEy6PE9EvqD7WY9uv0HMtlWwh44TGGt.jpg', NULL, NULL, NULL, '2024-02-04 12:04:48', '2024-02-04 12:06:44', NULL),
-(27, 'admin', 'Admin', 'admin@gmail.com', '2024-02-04 12:09:57', '$2y$12$ljLU8K3V8jojyY3dYoHul.8Sr93NIoz6Zvrtklh5x4WTkKGKJ2lKy', 2, 'images/default.png', NULL, NULL, NULL, '2024-02-04 12:09:54', '2024-02-04 12:09:54', NULL),
+(27, 'admin', 'Admin', 'admin@gmail.com', '2024-02-04 12:09:57', '$2y$12$gTsxj63FA/Pn9ZNa5YGaC.e7bKM/50XI0KdYOoV.f7xgpREiMS.C.', 2, 'avatar/y1atWZNdL8dEGWG9UszeaY3jMKWfKXvhj6ZvnAfE.png', NULL, NULL, NULL, '2024-02-04 12:09:54', '2024-02-23 06:57:53', NULL),
 (28, 'test7', 'asdasd', 'asdasdadsg4g@mail.com', NULL, '$2y$12$R7tRDg526Js39mz/brPKOevxgJE7BwaZRCuNYg.pkdh8F3W8L1xii', NULL, 'images/default.png', NULL, NULL, NULL, '2024-02-05 14:48:00', '2024-02-05 14:48:33', '2024-02-05 14:48:33'),
 (29, 'hiosdggiosdgjogojijipsdg', 'sdopkgipjsdgjip', 'asasgag@mail.com', NULL, '$2y$12$dTh/JFTZ4AB4xH6F/2CU5OniybIId/gr2ZLIHHoEd1bjto02.Nl/6', NULL, 'images/default.png', NULL, NULL, NULL, '2024-02-06 02:28:05', '2024-02-06 02:28:05', NULL);
 
@@ -387,25 +390,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `bans`
 --
 ALTER TABLE `bans`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT для таблицы `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT для таблицы `donates`
 --
 ALTER TABLE `donates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `donate_lists`
 --
 ALTER TABLE `donate_lists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `failed_jobs`
@@ -429,7 +432,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
